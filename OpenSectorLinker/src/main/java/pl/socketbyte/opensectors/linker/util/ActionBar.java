05eb9c9597efea2d4a08872bc4f1b7213e7066cf
SocketByte <poczta.xvacuum@gmail.com>
2018-03-05 13:08:12 +0100

    fixed pom to use only bukkit api, and temp actionbar, should work 1.8-1.12

--- OpenSectorLinker/src/main/java/pl/socketbyte/opensectors/linker/util/ActionBar.java
+++ OpenSectorLinker/src/main/java/pl/socketbyte/opensectors/linker/util/ActionBar.java
@@ -1,24 +1,14 @@
 package pl.socketbyte.opensectors.linker.util;
 
-import net.minecraft.server.v1_8_R3.IChatBaseComponent;
-import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
 import org.bukkit.ChatColor;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import pl.socketbyte.opensectors.linker.Linker;
 
 public class ActionBar {
 
-    // Totally forgot about these
-    // Reflection version coming soon xD sorry xD 
-
     public static void send(Player player, String text) {
-        PacketPlayOutChat packet = new PacketPlayOutChat(
-                IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + ChatColor.translateAlternateColorCodes('&', text) + "\"}"), (byte) 2);
-
-        ((CraftPlayer) player).getHandle()
-                .playerConnection.sendPacket(packet);
+        new io.github.theluca98.textapi.ActionBar(text).send(player);
     }
 
     public static void send(Player player, String text, int seconds) {
